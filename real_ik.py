12836489226ba517b2e7afd96eb64ea8377d1549
chanhyuk-jung <seagullcjung@gmail.com>
2026-09-08 16:44:08 +0900

    add piper python scripts

---
+++ real_ik.py
@@ -0,0 +1,119 @@
+import time
+
+import numpy as np
+import placo
+from ischedule import run_loop, schedule
+from placo_utils.tf import tf
+from placo_utils.visualization import frame_viz, robot_frame_viz, robot_viz
+from pyAgxArm import AgxArmFactory, ArmModel, PiperFW, create_agx_arm_config
+from scipy.spatial.transform import Rotation as R
+
+"""
+6axis robot reaching a given target (introduction example).
+"""
+
+# Loading the robot
+robot = placo.RobotWrapper("piper", placo.Flags.ignore_collisions)
+
+# Creating the solver
+solver = placo.KinematicsSolver(robot)
+
+solver.mask_fbase(True)
+solver.enable_velocity_limits(True)
+
+effector_task = solver.add_frame_task("joint6", np.eye(4))
+effector_task.configure("joint6", "soft", 1.0, 1.0)
+
+viz = robot_viz(robot)
+
+t = 0
+dt = 0.001
+solver.dt = dt
+
+cfg = create_agx_arm_config(
+    robot=ArmModel.PIPER, firmeware_version=PiperFW.DEFAULT, channel="can0"
+)
+
+arm = AgxArmFactory.create_arm(cfg)
+end_effector = arm.init_effector(arm.OPTIONS.EFFECTOR.AGX_GRIPPER)
+
+arm.connect()
+
+time.sleep(0.1)
+
+arm.enable()
+arm.set_speed_percent(100)
+
+ja = arm.get_joint_angles()
+gs = end_effector.get_gripper_status()
+
+for i in range(6):
+    robot.set_joint(f"joint{i + 1}", ja.msg[i])
+
+robot.set_joint("gripper_joint1", gs.msg.value / 2)
+robot.set_joint("gripper_joint2", -gs.msg.value / 2)
+
+robot.update_kinematics()
+
+m = tf.translation_matrix([0.3, 0.2 * np.sin(t), 0.2])
+
+rotation = R.from_euler("xyz", [0, -180, 0], degrees=True)
+m[:3, :3] = rotation.as_matrix()
+
+effector_task.T_world_frame = m
+
+viz.display(robot.state.q)
+robot_frame_viz(robot, "joint6")
+frame_viz("target", effector_task.T_world_frame)
+
+for _ in range(100):
+    time.sleep(dt)
+
+    m = tf.translation_matrix([0.3, 0.2 * np.sin(t), 0.2])
+
+    rotation = R.from_euler("xyz", [0, -180, 0], degrees=True)
+    m[:3, :3] = rotation.as_matrix()
+
+    effector_task.T_world_frame = m
+
+    # Solving the IK
+    solver.solve(True)
+    robot.update_kinematics()
+
+    state = []
+    for i in range(6):
+        state.append(robot.get_joint(f"joint{i + 1}"))
+
+    viz.display(robot.state.q)
+    robot_frame_viz(robot, "joint6")
+    frame_viz("target", effector_task.T_world_frame)
+
+
+@schedule(interval=dt)
+def loop():
+    global t
+    t += dt
+
+    m = tf.translation_matrix([0.2, 0.2 * np.sin(t), 0.2])
+
+    rotation = R.from_euler("xyz", [0, -180, 0], degrees=True)
+    m[:3, :3] = rotation.as_matrix()
+
+    effector_task.T_world_frame = m
+
+    # Solving the IK
+    solver.solve(True)
+    robot.update_kinematics()
+
+    state = []
+    for i in range(6):
+        state.append(robot.get_joint(f"joint{i + 1}"))
+
+    arm.move_j(state)
+
+    viz.display(robot.state.q)
+    robot_frame_viz(robot, "joint6")
+    frame_viz("target", effector_task.T_world_frame)
+
+
+run_loop()
---
+++ sim_ik.py
@@ -0,0 +1,49 @@
+import numpy as np
+import placo
+from ischedule import run_loop, schedule
+from placo_utils.tf import tf
+from placo_utils.visualization import frame_viz, robot_frame_viz, robot_viz
+from scipy.spatial.transform import Rotation as R
+
+# Loading the robot
+robot = placo.RobotWrapper("piper", placo.Flags.ignore_collisions)
+
+# Creating the solver
+solver = placo.KinematicsSolver(robot)
+
+solver.mask_fbase(True)
+solver.enable_velocity_limits(True)
+
+effector_task = solver.add_frame_task("flange_joint", np.eye(4))
+effector_task.configure("flange_joint", "soft", 1.0, 1.0)
+
+viz = robot_viz(robot)
+
+t = 0
+dt = 0.01
+solver.dt = dt
+
+
+@schedule(interval=dt)
+def loop():
+    global t
+    t += dt
+
+    m = tf.translation_matrix([0.3, 0.2 * np.sin(t), 0.2])
+
+    rotation = R.from_euler("xyz", [0, -180, 0], degrees=True)
+    m[:3, :3] = rotation.as_matrix()
+
+    effector_task.T_world_frame = m
+
+    # Solving the IK
+    solver.solve(True)
+    robot.update_kinematics()
+
+    # Displaying the robot, effector and target
+    viz.display(robot.state.q)
+    robot_frame_viz(robot, "flange_joint")
+    frame_viz("target", effector_task.T_world_frame)
+
+
+run_loop()
