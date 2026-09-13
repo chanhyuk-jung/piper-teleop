76648360a4bca8c395c377d3ca660a6319277e38
chanhyuk-jung <seagullcjung@gmail.com>
2026-09-14 07:41:01 +0900

    test on robot, sending & receiving works

--- piper_utils.py
+++ piper_utils.py
@@ -1,18 +1,19 @@
+from __future__ import annotations
+
 import time
 from dataclasses import dataclass
 
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
-
 from piper_sdk import C_PiperInterface_V2 as PiperBus
 
-NO_CONTROL = 0x00
+STANDBY = 0x00
 CAN_CONTROL = 0x01
 
-JOINT_MODE = 0x01
+MOVE_J = 0x01
 
-GRIPPER_DISABLE = 0x00
-GRIPPER_ENABLE = 0x01
+DISABLE = 0x00
+ENABLE = 0x01
 
 EMERGENCY_STOP = 0x01
 RESUME_EMERGENCY_STOP = 0x02
@@ -39,17 +40,43 @@ class JointState:
 
 
 class Piper:
-    def __init__(self, can: str, write_only: bool = False, timeout: float = 1.0):
+    def __init__(self, can: str, timeout: float = 1.0):
         self.bus: PiperBus = PiperBus(can_name=can)
 
-        self.bus.ConnectPort(start_thread=not write_only)
-        time.sleep(0.1)
+        self.bus.ConnectPort()
 
-        self.reset()
-        time.sleep(0.1)
-
-        self.enable_torque(timeout)
         self.enable_control()
+
+        self.enable_arm_torque()
+        self.enable_gripper_torque()
+
+    def enable_control(self):
+        self.bus.MotionCtrl_2(CAN_CONTROL, MOVE_J)
+
+    def set_speed(self, speed: int = 50):
+        self.bus.MotionCtrl_2(CAN_CONTROL, MOVE_J, speed)
+
+    def enable_arm_torque(self, timeout: float = 1.0):
+        self.bus.EnableArm()
+
+        t0 = time.perf_counter()
+        while not all(self.recv_arm_enable_status()):
+            if time.perf_counter() - t0 >= timeout:
+                raise TimeoutError("timeout exceeded while trying to enable arm")
+
+            self.bus.EnableArm()
+            time.sleep(0.1)
+
+    def disable_arm_torque(self, timeout: float = 1.0):
+        self.bus.DisableArm()
+
+        t0 = time.perf_counter()
+        while not any(self.recv_arm_enable_status()):
+            if time.perf_counter() - t0 >= timeout:
+                raise TimeoutError("timeout exceeded while trying to disable arm")
+
+            self.bus.DisableArm()
+            time.sleep(0.1)
 
     def recv_arm_enable_status(self) -> tuple[bool, ...]:
         msg = self.bus.GetArmLowSpdInfoMsgs()
@@ -66,17 +93,7 @@ class Piper:
         enable_status = tuple(motor.foc_status.driver_enable_status for motor in motors)
         return enable_status
 
-    def enable_torque(self, timeout: float = 1.0):
-        self.bus.EnableArm()
-
-        t0 = time.perf_counter()
-        while not all(self.recv_arm_enable_status()):
-            if time.perf_counter() - t0 >= timeout:
-                raise TimeoutError("timeout exceeded while trying to enable arm")
-
-            self.bus.EnableArm()
-            time.sleep(0.1)
-
+    def enable_gripper_torque(self, timeout: float = 1.0):
         msg = self.bus.GetArmGripperMsgs()
 
         g = msg.gripper_state
@@ -90,23 +107,13 @@ class Piper:
             if time.perf_counter() - t0 >= timeout:
                 raise TimeoutError("timeout exceeded while trying to enable gripper")
 
-            self.bus.GripperCtrl(pos, torque, GRIPPER_ENABLE)
+            self.bus.GripperCtrl(pos, torque, ENABLE)
             time.sleep(0.1)
 
             msg = self.bus.GetArmGripperMsgs()
             enabled = msg.gripper_state.foc_status.driver_enable_status
 
-    def disable_torque(self, timeout: float = 1.0):
-        self.bus.DisableArm()
-
-        t0 = time.perf_counter()
-        while not any(self.recv_arm_enable_status()):
-            if time.perf_counter() - t0 >= timeout:
-                raise TimeoutError("timeout exceeded while trying to disable arm")
-
-            self.bus.DisableArm()
-            time.sleep(0.1)
-
+    def disable_gripper_torque(self, timeout: float = 1.0):
         msg = self.bus.GetArmGripperMsgs()
 
         g = msg.gripper_state
@@ -120,46 +127,31 @@ class Piper:
             if time.perf_counter() - t0 >= timeout:
                 raise TimeoutError("timeout exceeded while trying to disable gripper")
 
-            self.bus.GripperCtrl(pos, torque, GRIPPER_DISABLE)
+            self.bus.GripperCtrl(pos, torque, DISABLE)
             time.sleep(0.1)
 
             msg = self.bus.GetArmGripperMsgs()
             enabled = msg.gripper_state.foc_status.driver_enable_status
 
-    def enable_control(self):
-        self.bus.MotionCtrl_2(CAN_CONTROL, JOINT_MODE)
-
-    def disable_control(self):
-        self.bus.MotionCtrl_2(NO_CONTROL)
-
-    def set_speed(self, speed: int):
-        speed = 100
-        self.bus.ModeCtrl(0x01, 0x01, speed, 0x00)
-
-    def close(self):
-        self.bus.DisconnectPort()
-
     def reset(self):
+        # clears any errors or e-stops or teaching modes, moves on second move command
         self.bus.MotionCtrl_1(
             RESUME_EMERGENCY_STOP, DISABLE_TRAJECTORY_CONTROL, DISABLE_TEACH
         )
 
     def emergency_stop(self):
+        # ignores move commands & disables torque
         self.bus.MotionCtrl_1(EMERGENCY_STOP)
 
     def unlock(self):
-        self.bus.EmergencyStop(RESUME_EMERGENCY_STOP)
+        # unlocks emergency stop, moves on second move command
+        self.bus.MotionCtrl_1(RESUME_EMERGENCY_STOP)
 
     def send_qpos(self, qpos: ArrayLike):
         qpos = np.asarray(qpos, dtype=np.float64)
+        qpos = np.rad2deg(qpos)
 
-        assert len(qpos) == 6, (
-            "qpos must have 6 or 7 joints where 7 includes the gripper"
-        )
-
-        qpos = np.rad2deg(qpos) * 1_000
-
-        j: list[int] = list(qpos.astype(np.int32))
+        j: tuple[int, ...] = tuple((qpos * 1000).astype(np.int32))
 
         self.bus.JointCtrl(
             joint_1=j[0],
@@ -170,11 +162,9 @@ class Piper:
             joint_6=j[5],
         )
 
-    def send_ee(self, width: float, force: float = 1.0):
-        self.bus.GripperCtrl(int(width * 1_000_000), int(force * 1_000), GRIPPER_ENABLE)
-
     def recv_q(self) -> JointState:
         msg = self.bus.GetArmHighSpdInfoMsgs()
+
         timestamp = msg.time_stamp
         hz = msg.Hz
 
@@ -195,6 +185,9 @@ class Piper:
 
         return q
 
+    def send_ee(self, width: float, torque: float = 1.0):
+        self.bus.GripperCtrl(int(width * 1_000_000), int(torque * 1_000), ENABLE)
+
     def recv_ee(self) -> GripperState:
         msg = self.bus.GetArmGripperMsgs()
 
@@ -206,17 +199,24 @@ class Piper:
         torque = state.grippers_effort / 1_000
 
         ee = GripperState(timestamp, hz, width, torque)
-
         return ee
+
+    def close(self):
+        self.bus.DisconnectPort()
 
 
 if __name__ == "__main__":
     piper = Piper("can0")
 
-    piper.send_qpos([0] * 6)
-    piper.send_ee(0)
-
-    time.sleep(4)
-
     q = piper.recv_q()
     ee = piper.recv_ee()
+
+    time.sleep(1)
+
+    piper.send_qpos(q.pos)
+    piper.send_ee(ee.width, ee.torque)
+
+    time.sleep(1)
+
+    piper.send_qpos([0] * 6)
+    piper.send_ee(0, 1)
